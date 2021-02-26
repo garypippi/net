1a57f81663e7454d83eb0c8497eb0c962231a81a
Naruto Ishikawa <garypippi@gmail.com>
2021-02-27 01:24:26 +0900

    wip

---
+++ modules/md/lexer/lexer.ts
@@ -0,0 +1,19 @@
+import { tick, task, Token } from './token'
+
+export const lexer = (input: string): Token[] => {
+    return tick([], task[0], input)
+        .filter(token => token.content.trim().length > 0)
+        .map(token => ({
+            ...token,
+            tokens: tick([], task[1], token.content)
+        }))
+    // return tick(t, u, input)
+    //     .filter(token => token.content.trim().length > 0)
+}
+
+// export const lexer = (input: string): Token[] => {
+//     return task.reduce<Token[]>((t, u) => {
+//         return tick(t, u, input)
+//     }, [])
+//         .filter(token => token.content.trim().length > 0)
+// }
